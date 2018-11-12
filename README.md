28d770c34724d958fbc42d0b1860818a20189c95
xvjialing <xvjialing@outlook.com>
2018-11-12 20:28:02 +0800

    commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# README
+
+构建docker镜像命令：`mvn clean package -Dmaven.test.skip=true`
+
+运行命令：`docker-compose up -d`
---
+++ src/main/java/com/xvjialing/demo/dockerlimitmemory/DockerLimitMemoryApplication.java
@@ -0,0 +1,12 @@
+package com.xvjialing.demo.dockerlimitmemory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerLimitMemoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerLimitMemoryApplication.class, args);
+    }
+}
